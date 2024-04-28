da7e2eaeefa65f69bac4a5580c70374a58875c41
kaanuzun <uzunkaana@gmail.com>
2024-04-28 22:01:57 +0300

    [SWIFT] - Xcode - Simple Storyboard UI Design

--- DesingWork/ViewController.swift
+++ DesingWork/ViewController.swift
@@ -11,7 +11,24 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    self.navigationItem.title = "Pizza"
+
+    let appearance = UINavigationBarAppearance()
+    appearance.backgroundColor = UIColor(named: "MainColor")
+    appearance.titleTextAttributes = [.foregroundColor: UIColor(
+      named: "LetterColor1")!,
+      .font: UIFont(
+        name: "Pacifico-Regular",
+        size: 22)!
+    ]
+
+    navigationController?.navigationBar.standardAppearance = appearance
+    navigationController?.navigationBar.compactAppearance = appearance
+    navigationController?.navigationBar.scrollEdgeAppearance = appearance
+
+    //Leading-> Left   - Android(Start)
+    //Trailing-> Right - Android(End)
   }
 
 
